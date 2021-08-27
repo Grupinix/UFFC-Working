f2f44f49dab20e5e0c1d1ebb435e7a35a1a20aaf
yurinogueira <yurinogueira@id.uff.br>
2021-08-27 16:03:20 -0300

    Setup more materials

--- Assets/Scripts/UserData/UserDeck.cs
+++ Assets/Scripts/UserData/UserDeck.cs
@@ -19,6 +19,15 @@ namespace UserData {
             new CardStatus(true, new[] {1, 1, 0}, new[] {0, 0, 0, 0}, 0, 0, 2),
             new CardStatus(true, new[] {1, 1, 1}, new[] {0, 0, 0, 0}, 0, 0, 1),
             new CardStatus(true, new[] {0, 1, 1}, new[] {0, 0, 0, 0}, 0, 0, 2),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 0, 0, 2}, 3, 2, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 0, 0, 1}, 2, 1, 2),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 0, 0, 2}, 2, 3, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {1, 0, 0, 1}, 3, 3, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {3, 0, 0, 0}, 4, 6, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 2, 0, 0}, 2, 4, 2),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 0, 0, 3}, 0, 8, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {0, 2, 0, 3}, 9, 5, 1),
+            new CardStatus(false, new[] {0, 0, 0}, new[] {2, 0, 0, 2}, 7, 5, 1),
         };
 
         public static readonly List<int> DeckCardIds = new List<int>();
