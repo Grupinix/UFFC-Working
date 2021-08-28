ead79c7ec13607ac70bbbf6144da02c7e62ba676
yurinogueira <yurinogueira@id.uff.br>
2021-08-28 03:41:55 -0300

    Add logo and start to create battle system

--- Assets/Scripts/Room/UserInterface.cs
+++ Assets/Scripts/Room/UserInterface.cs
@@ -4,14 +4,34 @@ using UnityEngine.UI;
 
 namespace Room {
     public class UserInterface : MonoBehaviour {
+        
+        private int _actualCardId;
 
+        [SerializeField] private int life = 20;
         [SerializeField] private List<Material> materials;
         [SerializeField] private GameObject cardInfoPanel;
         [SerializeField] private Image cardImage;
+        [SerializeField] private Slider barraDeVida;
+        [SerializeField] private Text textoVida;
 
         public void openCardView(int cardId) {
+            _actualCardId = cardId;
             cardInfoPanel.SetActive(true);
             cardImage.material = materials[cardId];
+        }
+
+        public void castCard() {
+            
+        }
+
+        public void removerVida(int amount) {
+            life -= amount;
+            textoVida.text = life.ToString();
+            barraDeVida.value = life;
+        }
+
+        public void closeGame() {
+            Application.Quit();
         }
 
         public void closePanel() {
