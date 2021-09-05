8b06a6c4dab0cc35cd6c7bd47d529594751deeae
yurinogueira <yurinogueira@id.uff.br>
2021-09-05 00:45:21 -0300

    Redefine Event

--- Assets/Scripts/APIs/FirebaseInit.cs
+++ Assets/Scripts/APIs/FirebaseInit.cs
@@ -2,6 +2,7 @@ using Firebase;
 using Firebase.Analytics;
 using Firebase.Extensions;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace APIs {
     
@@ -11,13 +12,16 @@ namespace APIs {
      * todas as dependencias existentes
      */
     public class FirebaseInit : MonoBehaviour {
-        
+
+        public UnityEvent OnFirebaseInitialized = new UnityEvent();
+
         private void Start() {
             FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
                 if (DependencyStatus.Available != task.Result) {
                     return;
                 }
                 FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+                OnFirebaseInitialized.Invoke();
             });
         }
     }
